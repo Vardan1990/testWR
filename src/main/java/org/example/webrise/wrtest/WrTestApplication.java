0f3a9351533338c5768c17e564ac11bf3e33040e
Vardan <vardanmartirosyan@bk.ru>
2025-05-09 02:56:58 +0400

    commit

---
+++ src/main/java/org/example/webrise/wrtest/WrTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.webrise.wrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrTestApplication.class, args);
+    }
+
+}
